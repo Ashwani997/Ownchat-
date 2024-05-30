347bf890fd9efa2db6d26a69ff2bf13ce96a6961
Ashwani997 <chauhanashwani971@gmail.com>
2024-05-30 12:24:00 +0530

    files added

--- index.js
+++ index.js
@@ -6,24 +6,8 @@ const { config } = require('dotenv');
 config();
 //import the GoogleGenerativeAI
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-//generate a API key
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-//Import the model 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-async function run() {
-  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-
-  const prompt = "write a essay on Ai."
-
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-}
-
-run();
-
+//import 
+const model = require("./geminiapi/user");
 const path = require('path');
 const cookieParser = require('cookie-parser');
 //import the mongodb
